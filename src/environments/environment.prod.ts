778b6e9a2dcb0793a5f0d0a9667db84d36e33ef1
Edu58 <edumuriithi58@gmail.com>
2022-04-12 12:11:53 +0300

    adds boilerplate

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'b54622adfbfa9d446c89a603c2696cba'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'b54622adfbfa9d446c89a603c2696cba'
 };
 
 /*
